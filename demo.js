2e2e209df8b5b18bca555580bd49e59a578da2a0
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-19 12:17:29 +0530

    unknown type

--- demo.js
+++ demo.js
@@ -14,3 +14,23 @@ var list1 = [1, 2, 3];
 var list2 = [1, 2, 3];
 //tuples
 var person1 = ["sakthi", 24];
+//enum
+var color;
+(function (color) {
+    color[color["red"] = 5] = "red";
+    color[color["green"] = 6] = "green";
+    color[color["blue"] = 7] = "blue";
+})(color || (color = {}));
+var c = color.blue;
+console.log(c);
+//unknown type
+var myvariable = 10;
+myvariable = "sakthi";
+// console.log(myvariable);
+function hashName(obj) {
+    return !!obj && typeof obj === "object" && "name" in obj;
+}
+if (hashName(myvariable)) {
+    console.log(myvariable.name);
+}
+myvariable.toUpperCase();
--- demo.ts
+++ demo.ts
@@ -24,3 +24,31 @@ let list1: number[] = [1, 2, 3];
 let list2: Array<number> = [1, 2, 3];
 //tuples
 let person1: [string, number] = ["sakthi", 24];
+
+//enum
+
+enum color {
+  red = 5,
+  green,
+  blue,
+}
+
+let c: color = color.blue;
+console.log(c);
+
+//unknown type
+
+let myvariable: unknown = 10;
+
+myvariable = "sakthi";
+
+// console.log(myvariable);
+function hashName(obj: any): obj is { name: string } {
+  return !!obj && typeof obj === "object" && "name" in obj;
+}
+
+if (hashName(myvariable)) {
+  console.log(myvariable.name);
+}
+
+(myvariable as string).toUpperCase();
