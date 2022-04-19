5e8a891504334c979342d212dd4db0a986fae7ef
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-19 11:11:37 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+// variable declaration
+var x = 10;
+var y = 20;
+var title = "type script learning";
+//variable datatype
+var name = "sakthivel";
+var age = 24;
+var employe = true;
+var sentence = "my name is ".concat(name, ",and my age is ").concat(age);
+console.log(sentence);
---
+++ demo.ts
@@ -0,0 +1,20 @@
+export {};
+
+// variable declaration
+
+let x = 10;
+const y = 20;
+
+const title = "type script learning";
+
+//variable datatype
+
+const name: string = "sakthivel";
+
+const age: Number = 24;
+
+const employe: boolean = true;
+
+const sentence: string = `my name is ${name},and my age is ${age}`;
+
+console.log(sentence);
